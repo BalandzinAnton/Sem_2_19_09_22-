67d18d0cbe06a2a9ee2dd3bc7daacc0a62b0cad4
Anton <balandzin2602@gmail.com>
2022-09-19 19:13:18 +0300

    add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите на вход два числа и установите является ли второе кратным первому числу
+using static System.Console;
+
+Clear();
+WriteLine("Введите числа: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+if(num2%num1==0)
+{
+    WriteLine($"Кратно");
+}
+{
+    WriteLine("На ноль делить нельзя");
+}
+else 
+{
+    WriteLine($"Остаток от деления: {num2%num1}");
+}
+
