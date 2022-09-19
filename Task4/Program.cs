0700c28617a73f98609fd55adc1dd8014ebe4acd
Anton <balandzin2602@gmail.com>
2022-09-19 19:16:52 +0300

    add Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,45 @@
+﻿// Программа принимает на вход два числа и установите является ли одно число квадратом другого
+using static System.Console;
+
+Clear();
+WriteLine("Введите числа: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 == num2)
+{
+   
+    WriteLine($"Число {num1} равно числу {num2}");
+    return;
+}
+if (num1 == 0 || num2 == 0)
+{
+   
+    WriteLine($"Выберите отличные от нуля значения");
+    return;
+}
+if (num1>num2)
+{
+    if (num1 == num2*num2)
+    {
+        WriteLine($"Число {num1} является квадратом {num2}");
+    }
+    else
+    {
+        WriteLine($"Число {num1} НЕ является квадратом {num2}");
+    }
+    return;
+}
+if (num2>num1)
+{
+    int temp = num1;
+    num1 = num2;
+    num2 = temp;
+    if (num1 == num2*num2)
+    {
+        WriteLine($"Число {num1} является квадратом {num2}");
+    }
+    else
+    {
+        WriteLine($"Число {num1} НЕ является квадратом {num2}");
+    }
+}
