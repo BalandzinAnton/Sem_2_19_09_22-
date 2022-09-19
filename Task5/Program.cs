40b3a44439a6f63ad0b7a34c9569a17f9479388a
Anton <balandzin2602@gmail.com>
2022-09-19 19:29:58 +0300

    add Task5

---
+++ Task5/Program.cs
@@ -0,0 +1,13 @@
+﻿// Необходимо развернуть трехзначное число наоборот
+
+using static System.Console;
+Clear();
+
+int number=new Random().Next(100,1000);
+WriteLine(number);
+int a3 = number%10;
+int a1 = number/100;
+int a2 = number/10-a1*10;
+int result = a3*100 + a2*10 + a1;
+
+WriteLine(result);
