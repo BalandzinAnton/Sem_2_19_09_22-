41d79be469cba0a119572fda524f98d43b7a1244
Anton <balandzin2602@gmail.com>
2022-09-19 18:38:43 +0300

    add

---
+++ Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Взять трехзначное число, вырезать из него вторую цифру и получить двухзначное
+
+using static System.Console;
+
+Clear();
+int number=new Random().Next(100,1000);
+WriteLine(number);
+int a0 = number%10;
+int a1 = number/100;
+
+int result = a1*10 + a0;
+
+WriteLine(result);
+
